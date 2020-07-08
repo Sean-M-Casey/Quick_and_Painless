1378053a8c8054f3cb4d34ae7bbfd9368bb70938
ZMReckless <zachmreckless@outlook.com>
2020-07-08 17:00:09 +1000

    Polter Pad tabs and pages
    Added Tabs for Glimpses, Suspects, Calendar and map with pages for each

--- Quick and Painless/Assets/Scripts/PolterPadScript.cs
+++ Quick and Painless/Assets/Scripts/PolterPadScript.cs
@@ -8,6 +8,8 @@ public class PolterPadScript : MonoBehaviour
     public GameObject polterPad;
     public GameObject polterPadUI;
     public GameObject[] ppPages;
+    public GameObject PageUp;
+    public GameObject PageDown;
     public UnityEvent triggerTimePause;
     public UnityEvent triggerTimeUnPause;
     public UnityEvent mouseLock;
@@ -34,28 +36,80 @@ public class PolterPadScript : MonoBehaviour
                 ppActive--;
                 mouseLock.Invoke();
             }
-        }    
+        }
+        for (int i = 0; i < 12; i++)
+        {
+            if (i != ppPage)
+            {
+                ppPages[i].SetActive(false);
+            }
+            else
+            {
+                ppPages[ppPage].SetActive(true);
+            }
+        }
     }
-    public void PageUp()
+    public void UpArrow()
     {
-        ppPage = 1;
-        ppPages[ppPage - 1].SetActive(false);
-        ppPages[ppPage].SetActive(true);
+            ppPage++;
+            ppPages[ppPage - 1].SetActive(false);
+            ppPages[ppPage].SetActive(true);
+        if (ppPage == 1 || ppPage == 5 || ppPage == 7 || ppPage == 8)
+        {
+            PageUp.SetActive(false);
+            PageDown.SetActive(true);
+        }
+        else
+        {
+            PageUp.SetActive(true);
+            PageDown.SetActive(true);
+        }
     }
-    public void PageDown()
+    public void DownArrow()
+    {
+            ppPage--;
+            ppPages[ppPage + 1].SetActive(false);
+            ppPages[ppPage].SetActive(true);
+        if (ppPage == 0 || ppPage == 2 || ppPage == 6 || ppPage == 8)
+        {
+            PageUp.SetActive(true);
+            PageDown.SetActive(false);
+        }
+        else
+        {
+            PageUp.SetActive(true);
+            PageDown.SetActive(true);
+        }
+    }
+    public void SuspectsTab()
     {
         ppPage = 0;
-        ppPages[ppPage + 1].SetActive(false);
-        ppPages[ppPage].SetActive(true);
+        PageDown.SetActive(false);
+        PageUp.SetActive(true);
     }
-    public void Gallery()
+    public void GlimpsesTab()
     {
-
+        ppPage = 2;
+        PageDown.SetActive(false);
+        PageUp.SetActive(true);
+    }
+    public void MapTab()
+    {
+        ppPage = 6;
+        PageDown.SetActive(false);
+        PageUp.SetActive(true);
+    }
+    public void CalendarTab()
+    {
+        ppPage = 8;
+        PageDown.SetActive(false);
+        PageUp.SetActive(false);
     }
     IEnumerator ActivatePP()
     {
         yield return new WaitForSeconds(0.2f);
         polterPad.SetActive(true);
+        PageDown.SetActive(false);
         polterPadUI.SetActive(false);
         triggerTimePause.Invoke();
         ppActive++;
